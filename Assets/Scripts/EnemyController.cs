2a598da211e7b8c4c145b8befbc40eef793bd8aa
Miguel Angel Ybañez Esquerre <arcangel080902@gmail.com>
2024-09-21 00:01:47 -0500

    1 video al mes

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform player; 
+    public float detectionRadius = 5.0f; 
+    public float speed = 2.0f;
+    public float fuerzaRebote = 6f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private bool enMovimiento;
+    private bool recibiendoDanio;
+    private Animator animator;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distanceToPlayer < detectionRadius)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+
+            if (direction.x < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            if (direction.x > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+
+            movement = new Vector2(direction.x, 0);
+
+            enMovimiento = true;
+        }
+        else
+        {
+            movement = Vector2.zero;
+            enMovimiento = false;
+        }
+        if(!recibiendoDanio)
+            rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+
+        animator.SetBool("enMovimiento", enMovimiento);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Vector2 direccionDanio = new Vector2(transform.position.x, 0);
+
+            collision.gameObject.GetComponent<PlayerController>().RecibeDanio(direccionDanio, 1);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Espada"))
+        {
+            Vector2 direccionDanio = new Vector2(collision.gameObject.transform.position.x, 0);
+
+            RecibeDanio(direccionDanio, 1);
+        }
+    }
+    public void RecibeDanio(Vector2 direccion, int cantDanio)
+    {
+        if (!recibiendoDanio)
+        {
+            recibiendoDanio = true;
+            Vector2 rebote = new Vector2(transform.position.x - direccion.x, 0.2f).normalized;
+            rb.AddForce(rebote * fuerzaRebote, ForceMode2D.Impulse);
+            StartCoroutine(DesactivaDanio());
+        }
+    }
+    IEnumerator DesactivaDanio()
+    {
+        yield return new WaitForSeconds(0.4f);
+        recibiendoDanio = false;
+        rb.velocity = Vector2.zero;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocidad = 5f;
+
+    public float fuerzaSalto = 10f; 
+    public float fuerzaRebote = 6f; 
+    public float longitudRaycast = 0.1f; 
+    public LayerMask capaSuelo; 
+
+    private bool enSuelo; 
+    private bool recibiendoDanio;
+    private bool atacando;
+    private Rigidbody2D rb; 
+
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!atacando)
+        {
+            Movimiento();
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, longitudRaycast, capaSuelo);
+            enSuelo = hit.collider != null;
+
+            if (enSuelo && Input.GetKeyDown(KeyCode.Space) && !recibiendoDanio)
+            {
+                rb.AddForce(new Vector2(0f, fuerzaSalto), ForceMode2D.Impulse);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z) && !atacando && enSuelo)
+        {
+            Atacando();
+        }
+
+        animator.SetBool("ensuelo", enSuelo);
+        animator.SetBool("recibeDanio", recibiendoDanio);
+        animator.SetBool("Atacando", atacando);
+    }
+       
+    public void Movimiento()
+    {
+        float velocidadX = Input.GetAxis("Horizontal") * Time.deltaTime * velocidad;
+
+        animator.SetFloat("movement", velocidadX * velocidad);
+
+        if (velocidadX < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (velocidadX > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        Vector3 posicion = transform.position;
+
+        if (!recibiendoDanio)
+            transform.position = new Vector3(velocidadX + posicion.x, posicion.y, posicion.z);
+    }
+    public void RecibeDanio(Vector2 direccion, int cantDanio)
+    {
+        if(!recibiendoDanio)
+        {
+            recibiendoDanio = true;
+            Vector2 rebote = new Vector2(transform.position.x - direccion.x, 0.2f).normalized;
+            rb.AddForce(rebote* fuerzaRebote, ForceMode2D.Impulse);
+        }
+    }
+
+    public void DesactivaDanio()
+    {
+        recibiendoDanio = false;
+        rb.velocity = Vector2.zero;
+    }
+
+    public void Atacando()
+    {
+        atacando = true;
+    }
+
+    public void DesactivaAtaque()
+    {
+        atacando = false;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * longitudRaycast);
+    }
+}
