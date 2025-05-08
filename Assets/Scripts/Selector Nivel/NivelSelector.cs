7c4fe5672da00e95749020dbf715b544d34b617d
Miguel Angel Ybañez Esquerre <109547479+Miguel080902@users.noreply.github.com>
2025-05-08 18:42:33 -0500

    equisde

---
+++ Assets/Scripts/Selector Nivel/NivelSelector.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NivelSelector : MonoBehaviour
+{
+    public GameObject nivelButtonPrefab; 
+    public Transform buttonContainer; 
+    public int totalLevels = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateLevelButtons();
+    }
+
+    void GenerateLevelButtons()
+    {
+        for (int i = 1; i <= totalLevels; i++)
+        {
+            GameObject buttonObj = Instantiate(nivelButtonPrefab, buttonContainer);
+            buttonObj.GetComponentInChildren<TextMeshProUGUI>().text = "Nivel " + i;
+
+            int levelIndex = i;
+            buttonObj.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                SceneManager.LoadScene("Nivel_" + levelIndex);
+            });
+        }
+    }
+}
